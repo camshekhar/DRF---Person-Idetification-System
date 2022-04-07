4b42075c44f39d3dfd65329d2b6865bae3b5d4c8
camshekhar <s.shekhar6057@gmail.com>
2022-04-07 16:15:40 +0530

    Settings Changed

--- PIS/settings.py
+++ PIS/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-ct8kj+ojaw3anxevctvt=f+(n(ne2yz1m=dlwfju5pz!9d&36#'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
